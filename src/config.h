0f83048956b97233b5bec377826d31a4626c84d0
Joshua Watkins <joshua@watkinsfamily.com.au>
2024-11-20 16:01:40 +1100

    Improvements: Put global variables in a Config struct to organise variables.

--- src/config.h
+++ src/config.h
@@ -1,17 +1,19 @@
 #ifndef CONFIG_H
 #define CONFIG_H
-    
+
 #define WINDOW_TITLE "Temperature Monitor"
 #define IDI_ICON1 101
 
-// IP related
-const char *ip = "192.168.0.37";
-int port = 5005;
+typedef struct
+{
+  const char *ip;
+  int port;
+  float globalTemperature;
+  float globalHumidity;
+  int packetCounter;
+  char connectionStatus[24];
+} Config;
 
-// Global variables for screen display
-static float globalTemperature = 0.0;
-static float globalHumidity = 0.0;
-int packetCounter = 0;
-char connectionStatus[64] = "Connecting...";
+extern Config config;
 
 #endif
