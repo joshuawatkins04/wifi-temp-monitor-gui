414bdc2ee21f2a3047bd7619664f9a5a74e96f8c
Joshua Watkins <joshua@watkinsfamily.com.au>
2024-11-20 18:56:01 +1100

    Additions: UDP receiver class to listen and interpret incoming packets. ContentView file displays this information received.

---
+++ ios/TempMonitorApp/TempMonitorApp/ContentView.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+
+struct ContentView: View {
+  @StateObject private var udpReceiver = UDPReceiver()
+
+  var body: some View {
+    VStack(spacing: 20) {
+      Text("Temperature:")
+        .font (.title)
+        .padding(.top, 50)
+      Text(udpReceiver.temperature)
+        .font(.system(size: 48))
+        .bold()
+        .foregroundColor(.blue)
+      
+      Text("Humidity")
+        .font(.title)
+      Text(udpReceiver.humidity)
+        .font(.system(size: 48))
+        .bold()
+        .foregroundColor(.green)
+
+      Spacer()
+    }
+    .padding()
+    .onAppear {
+      udpReceiver.startListening(port: 8080)
+    }
+  }
+}
---
+++ ios/TempMonitorApp/TempMonitorApp/UDPReceiver.swift
@@ -0,0 +1,55 @@
+import Network
+
+class UDPReceiver: ObservableObject {
+  @Published var temperature: String = "--"
+  @Published var humidity: String = "--"
+
+  private var listener: NWListener?
+
+  init(port: UInt16 = 8082) {
+    startListening(port: port)
+  }
+
+  func startListening(port: UInt16) {
+    do {
+      listener = try NWListener(using: .udp, on: NWEndpoint.Port(rawValue: port)!)
+      listener?.stateUpdateHandler = { state in 
+        print("Listening state: \(state)")
+      }
+      listener?.newConnectionHandler = { connection in
+        connection.start(queue: .main)
+        self.receive(on: connection)
+      }
+      listener?.start(queue: .main)
+    } catch {
+      print("Failed to start listener: \(error)")
+    }
+  }
+
+  private func receive(on connection: NWConnection) {
+    connection.receiveMessage { (data, _, isComplete, error) in
+      if let data = data, let message = String(data: data, encoding: .utf8) {
+        print("Received: \(message)")
+        self.parseMessage(message)
+      }
+      if isComplete {
+        connection.cancel()
+      }
+    }
+  }
+
+  private func parseMessage(_ message: String) {
+    let components = message.split(separator: ",")
+    
+    if components.count == 2,
+      let temperatureValue = Double(components[0].trimmingCharacters(in: .whitespaces)),
+      let humidityValue = Double(components[0].trimmingCharacters(in: .whitespaces)) {
+        DispatchQueue.main.async {
+          self.temperature = String(format: "%.2f°C", temperatureValue)
+          self.humidity = String(format: "%.2f%%", humidityValue)
+        }
+      } else {
+        printf("Invalid message format: \(message)")
+      }
+  }
+}
